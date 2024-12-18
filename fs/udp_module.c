e6a05e765be7d42faf5c59da45c828f1e084213b
sabaebrahimi <saba.eb18@gmail.com>
2024-12-18 06:48:25 -0700

    Strings fixed, Delete extra messages

--- fs/udp_module.c
+++ fs/udp_module.c
@@ -19,38 +19,6 @@ struct sockaddr_in remote_addr = {0};
 struct msghdr msg = {0};
 struct kvec iov;
 
-// int perform_udp_request(void);
-/*
-static int write_file_from_kernel(const char *path, const char *data, size_t size)
-{
-    struct file *filp;
-    loff_t pos = 0;
-    ssize_t bytes_written;
-
-    // Open (or create) the file with write-only mode. Using O_CREAT and O_TRUNC will overwrite if exists.
-    filp = filp_open(path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (IS_ERR(filp)) {
-        pr_err("Failed to open file: %s, error: %ld\n", path, PTR_ERR(filp));
-        return PTR_ERR(filp);
-    }
-
-    // Write data to the file
-    bytes_written = kernel_write(filp, data, size, &pos);
-    if (bytes_written < 0) {
-        pr_err("Failed to write to file: %s, error: %zd\n", path, bytes_written);
-        filp_close(filp, NULL);
-        return bytes_written;
-    }
-
-    pr_info("Successfully wrote %zd bytes to %s\n", bytes_written, path);
-
-    // Close the file
-    filp_close(filp, NULL);
-
-    return 0;
-}
-*/
-
 static int remote_storage_init(void) {
     printk(KERN_INFO "Network module loaded\n");
     int ret;
@@ -61,15 +29,9 @@ static int remote_storage_init(void) {
         return ret;
     }
 
-    printk(KERN_INFO "Socket created\n");
-
-    // sock->file->f_flags |= O_NONBLOCK;
-
     remote_addr.sin_family = AF_INET;
     remote_addr.sin_port = htons(DEST_PORT); // Set destination port
     remote_addr.sin_addr.s_addr = in_aton(DEST_IP);
-
-    printk(KERN_INFO "UDP Client: Destination address set to %s:%d\n", DEST_IP, DEST_PORT);
 
     return 0;
 }
@@ -92,7 +54,6 @@ int call_remote_storage(char* filename, size_t size, unsigned long index, char* 
 
     //Initialize the socket
     if (!sock) {
-        printk(KERN_INFO "Remote: UDP Client: Socket is not initialized. Initializing now...\n");
         ret = remote_storage_init();
         if (ret < 0) {
             printk(KERN_ERR "Remote: UDP Client: Failed to initialize socket, error %d\n", ret);
@@ -109,10 +70,8 @@ int call_remote_storage(char* filename, size_t size, unsigned long index, char* 
     ret = kernel_sendmsg(sock, &msg, &iov, 1, data_len);
     if (ret < 0) {
         printk(KERN_ERR "Remote: UDP Client: Failed to send message, error %d\n", ret);
-    } else {
-        printk(KERN_INFO "Remote: UDP Client: Successfully sent %d bytes to %s:%d\n", ret, DEST_IP, DEST_PORT);
+        goto error;
     }
-    kfree(data);
 
     iov.iov_base = buffer;
     iov.iov_len = 1024;
@@ -120,22 +79,16 @@ int call_remote_storage(char* filename, size_t size, unsigned long index, char* 
     ret = kernel_recvmsg(sock, &msg, &iov, 1, iov.iov_len, MSG_WAITALL);
     if (ret < 0 && ret != -EAGAIN) {
         printk(KERN_ERR "Remote: kernel_recvmsg failed: %d\n", ret);
-        return ret;
+        goto error;
     } else {
         if (ret > 0) {
             buffer[ret] = '\0';
-            pr_info("Remote: Received message: %s\n", buffer);
-            // char* path = "/tmp/";
-            // strcat(path, filename);
-            // write_file_from_kernel(path, buffer, size);
         }
     }
-    // kfree(buffer);
-    // Step 4: Clean up the socket
+error:
+    kfree(data);
     remote_storage_exit();
-    printk(KERN_INFO "Remote: UDP Client: Socket released\n");
-
-    return 0;
+    return ret;
 }
 EXPORT_SYMBOL(call_remote_storage);
 
